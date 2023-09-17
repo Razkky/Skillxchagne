87a4e28f5b2ddebae2f31bb9fe8764b21e494e6a
M0st4f4h4ss4n <m0st4f4h4ss4n@gmail.com>
2023-09-17 13:04:48 +0300

    dummy skills

--- src/client/src/components/profile/Profile.js
+++ src/client/src/components/profile/Profile.js
@@ -1,5 +1,9 @@
+
 import React, { useState, useEffect } from 'react';
-// import SkillsController from './SkillsController'; // Import the SkillsController
+import SkillsController from '../skills/skills';
+import { FaBook, FaChalkboardTeacher } from 'react-icons/fa'; // Import icons for skills
+
+const REACT_APP_BASE_URL = process.env.REACT_APP_BASE_URL;
 
 async function getUserProfile() {
   const token = localStorage.getItem('authToken');
@@ -8,7 +12,7 @@ async function getUserProfile() {
     return null;
   }
 
-  const response = await fetch('/api/user/', {
+  const response = await fetch(`${REACT_APP_BASE_URL}/api/user/`, {
     method: 'GET',
     headers: {
       'Authorization': `Bearer ${token}`
@@ -34,6 +38,7 @@ async function getUserProfile() {
   }
 }
 
+
 function UserProfile() {
   const [userProfile, setUserProfile] = useState(null);
   const [loading, setLoading] = useState(true);
@@ -49,18 +54,36 @@ function UserProfile() {
   }, []);
 
   return (
-    <div className="container mx-auto px-4 sm:px-6 lg:px-8 mt-10 sm:max-w-md md:max-w-lg lg:max-w-xl">
+    <div className="shadow-md rounded px-8 pt-6 pb-8 mb-4">
       {loading ? (
-        <p>Loading user profile...</p>
+        <p className="text-center">Loading user profile...</p>
       ) : userProfile ? (
         <div>
-          <h2>User Profile</h2>
-          <p>Full Name: {userProfile.fullName}</p>
-          <p>Email: {userProfile.email}</p>
-          {/* Display other user profile information here */}
+          <h2 className="text-2xl font-bold mb-4">User Profile</h2>
+          <p className="mb-4">
+            <span className="font-semibold">Full Name:</span> {userProfile.profile.fullName}
+          </p>
+          <p className="mb-4">
+            <span className="font-semibold">Email:</span> {userProfile.email}
+          </p>
+          <div className="mb-4">
+            <h3 className="text-lg font-semibold">Skills</h3>
+            <div className="flex items-center mb-2">
+              <FaBook className="text-blue-500 mr-2" /> {/* Book icon */}
+              <span className="font-semibold">Skills to Learn:</span>
+            </div>
+            <SkillsController skills={userProfile.skillsToLearn} /> {/* Pass skills data */}
+          </div>
+          <div>
+            <div className="flex items-center mb-2">
+              <FaChalkboardTeacher className="text-green-500 mr-2" /> {/* Teacher icon */}
+              <span className="font-semibold">Skills to Teach:</span>
+            </div>
+            <SkillsController skills={userProfile.skillsToTeach} /> {/* Pass skills data */}
+          </div>
         </div>
       ) : (
-        <p>No user profile data available.</p>
+        <p className="text-center">No user profile data available.</p>
       )}
     </div>
   );
