0f74fd35d0d1c18add29856a39a78b966cf3323d
M0st4f4h4ss4n <m0st4f4h4ss4n@gmail.com>
2023-09-17 18:29:59 +0300

    AppFix

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -23,7 +23,23 @@ function App() {
     <div className="bg-gradient-to-br from-gray-900 via-gray-800 to-gray-700 min-h-screen flex flex-col text-white">
       <Router>
         <Header isAuthenticated={isAuthenticated} setIsAuthenticated={setIsAuthenticated} />
-        {/* ... (rest of the code) */}
+        
+        <main className="container mx-auto flex flex-col md:flex-row p-4 mt-4">
+          <div className="w-full p-4 bg-gradient-to-br from-gray-500 via-gray-800 to-gray-700
+          rounded-lg shadow-lg">
+            <Routes>
+              <Route path="/" element={<Hero />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/signup" element={<SignupPage />} />
+              <Route path="/profile" element={<PrivateRoute><UserProfile /></PrivateRoute>} />
+            </Routes>
+          </div>
+        </main>
+        <footer className="bg-blue-600 text-white py-4 text-center mt-auto">
+          <div className="container mx-auto">
+            <p>&copy; {new Date().getFullYear()} My App. All rights reserved.</p>
+          </div>
+        </footer>
       </Router>
     </div>
   );
